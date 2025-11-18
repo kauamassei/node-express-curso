f7ec3469efb463fcb13d6a3f49376587b52ec450
kauamassei <gameplayultra11@gmail.com>
2025-11-18 03:02:59 -0300

    feat: add api routes get and post

--- exercises.js
+++ exercises.js
@@ -1,39 +1,68 @@
-import express, { json } from 'express'
+import express, { json } from "express";
+import { randomUUID } from "node:crypto";
 
-const app = express()
-const port = 3333
+const app = express();
+const port = 3333;
 
-app.get('/', (req, res) => {
-    res.send('Olá mundo')
-})
+app.use(express.json());
 
-app.get('/status', (req, res) => {
-    res.json({"status": "ok", "servidor": "online"})
-})
+const produtos = [];
 
-app.get('/users/:nome', (req, res) => {
-    const nome = req.params.nome
-    res.send(`Olá, ${nome}}`)
-})
+app.get("/", (req, res) => {
+  res.send("Olá mundo");
+});
 
-app.get('/soma', (req, res) => {
-    const {a, b} = req.query
+app.get("/status", (req, res) => {
+  res.json({ status: "ok", servidor: "online" });
+});
 
-    const numA = Number(a)
-    const numB = Number(b)
+app.get("/users/:nome", (req, res) => {
+  const nome = req.params.nome; //armazenando o parametro na variavel nome
+  res.send(`Olá, ${nome}}`);
+});
 
-    if(isNaN(numA) || isNaN (numB)){
-        return res.status(400).json({erro: 'Os parâmetros precisam ser números'})
-    }
+app.get("/soma", (req, res) => {
+  const { a, b } = req.query; // desestruturando as variaveis que vou receber na query
 
-    const result = numA + numB
+  const numA = Number(a); // convertendo para numero
+  const numB = Number(b);
 
-    res.status(200).json({'resultado': result })
-    
-})
+  if (isNaN(numA) || isNaN(numB)) {
+    return res.status(400).json({ erro: "Os parâmetros precisam ser números" }); //validacao
+  }
 
+  const result = numA + numB;
 
+  res.status(200).json({ resultado: result });
+});
+
+app.post("/produtos", (req, res) => {
+  const { nome, preco } = req.body; // traz os conteúdos e propriedas do 'corpo' da requisicao
+  const productId = randomUUID();
+
+  if (!productId || !nome || !preco) {
+    return res.status(400).json({ erro: "Envie 'nome' e 'preco'" }); // validacao se há ou não o preco e o nome inseridos
+  }
+
+  produtos.push({ productId, nome, preco });
+
+  res.status(201).json({ mensagem: "Produto criado" });
+});
+
+app.get("/produtos", (req, res) => {
+  res.json(produtos);
+});
+
+app.get("/produtos/:id", (req, res) => {
+  const { id } = req.params;
+  const product = produtos.find((p) => p.productId === id); // pega um unico produto e compara o seu id com o da url
+
+  if (!product) {
+    return res.status(404).json({ erro: "Produto não encontrado" }); 
+  }
+  res.json(product);
+});
 
 app.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`)
-})
+  console.log(`Servidor rodando na porta ${port}`);
+});
